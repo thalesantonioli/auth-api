24b432eec9f3915c03cacdd03d6e9224a89e58e9
yurilenzibartochevis <yuri.bartochevis@hotmail.com>
2022-03-23 23:51:36 -0300

    fix remote sqs

--- src/main/kotlin/com/fiap/hmv/configuration/SQSConfiguration.kt
+++ src/main/kotlin/com/fiap/hmv/configuration/SQSConfiguration.kt
@@ -2,7 +2,6 @@ package com.fiap.hmv.configuration
 
 import com.amazonaws.auth.AWSCredentialsProvider
 import com.amazonaws.auth.DefaultAWSCredentialsProviderChain
-import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.sqs.AmazonSQSAsync
 import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder
 import org.springframework.beans.factory.annotation.Value
@@ -13,20 +12,14 @@ import org.springframework.context.annotation.Primary
 @Configuration
 class SQSConfiguration {
 
-    @Value("\${cloud.aws.region:eu-central-1}")
+    @Value("\${cloud.aws.region:sa-east-1}")
     private val awsRegion: String? = null
-
-    @Value("\${cloud.aws.endpoint}")
-    private val awsEndpoint: String? = null
 
     @Bean
     @Primary
     fun amazonSNSAsync(): AmazonSQSAsync {
         return AmazonSQSAsyncClientBuilder.standard()
             .withCredentials(credentialsProvider())
-            .withEndpointConfiguration(
-                AwsClientBuilder.EndpointConfiguration(awsEndpoint, awsRegion)
-            )
             .build()
     }
 
