927e25bf89df25e3b7a2c22d55b9b46e71036158
yurilenzibartochevis <yuri.bartochevis@hotmail.com>
2022-03-27 16:10:04 -0300

    Fix CORS problem

--- src/main/kotlin/com/fiap/hmv/repository/UserRepository.kt
+++ src/main/kotlin/com/fiap/hmv/repository/UserRepository.kt
@@ -1,7 +1,6 @@
 package com.fiap.hmv.repository
 
 import com.fiap.hmv.model.persistence.User
-import org.bson.types.ObjectId
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
